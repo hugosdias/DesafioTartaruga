34c00ec4b22bfbcfb8758dc05e57476370bbb210
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-10-08 15:12:22 -0300

    desafio resolvido

--- Program.cs
+++ Program.cs
@@ -10,23 +10,21 @@ class Desafio {
       if (numeroQuantidade >= 1 && numeroQuantidade <= 500) {
         string[] tartarugas = Console.ReadLine().Split(" ");
         var maiorVelocidade = Int32.Parse(tartarugas[0]);
+          
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for ( int i = 0; i < numeroQuantidade; i++) {
+          var tartaruga = Int32.Parse(tartarugas[i]);
+            
+          if ( tartaruga > maiorVelocidade) {
+            maiorVelocidade = tartaruga;
+          }
+        }
 
-                // TODO: Crie as outras condições necessárias para a resolução do desafio:
-                for (int i = 0; i <= quantidadeEntradas; i++)
-                {
-                    var tartaruga = Int32.Parse(tartarugas[i]);
-
-                    if (                                          )
-                    {
-                        maiorVelocidade = tartaruga;
-                    }
-                }
-
-                if(                            < 10) {
+        if( maiorVelocidade < 10) {
           Console.WriteLine(1);
-        } else if (                            >= 10 &&                            < 20) {
+        } else if (maiorVelocidade >= 10 && maiorVelocidade < 20) {
           Console.WriteLine(2);
-        } else if (                                        >= 20) {
+        } else if (maiorVelocidade >= 20) {
           Console.WriteLine(3);
         }
         quantidadeEntradas--;
